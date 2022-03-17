fd8707f0794ec582c536bb799d5e9001daa34caf
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-03-17 10:23:35 +0530

    Update create-mgmt-cluster.sh

--- cluster-ops/create-mgmt-cluster.sh
+++ cluster-ops/create-mgmt-cluster.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 read -p 'mgmtClusterName: ' mgmtClusterName
 export $mgmtClusterName
-eksctl anywhere create cluster -f /home/ubuntu/cluster-configs/eks-a-$mgmtClusterName-cluster.yaml
+eksctl anywhere create cluster -f /home/ubuntu/cluster-configs/$mgmtClusterName-eks-a-cluster.yaml
