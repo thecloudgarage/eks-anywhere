7d99040eb4e0bea63a03172d66dbc74cd4806675
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-03-17 12:40:15 +0530

    Update create-workload-cluster.sh

--- cluster-ops/create-workload-cluster.sh
+++ cluster-ops/create-workload-cluster.sh
@@ -1,10 +1,12 @@
 #!bin/bash
 read -p 'workloadClusterName: ' workloadClusterName
 read -p 'mgmtClusterName: ' mgmtClusterName
+read -p 'staticIp: ' staticIp
 cd /home/ubuntu/
 cp /home/ubuntu/eks-anywhere/cluster-samples/workload-eks-a-cluster-sample.yaml \ 
    /home/ubuntu/cluster-configs/$workloadClusterName-eks-a-cluster.yaml
-sed -i 's/w01/$workloadClusterName/g' 
+sed -i 's/w01/$workloadClusterName/g' $workloadClusterName-eks-a-cluster.yaml
+sed -i 's/staticIp/$staticIp/g' $workloadClusterName-eks-a-cluster.yaml 
 eksctl anywhere create cluster \
-  -f /home/ubuntu/cluster-configs/$workloadClusterName-eks-a-cluster.yaml  \
-  --kubeconfig $mgmtClusterName/$mgmtClusterName-eks-a-cluster.kubeconfig
+  -f /home/ubuntu/$workloadClusterName-eks-a-cluster.yaml  \
+  --kubeconfig /home/ubuntu/$mgmtClusterName/$mgmtClusterName-eks-a-cluster.kubeconfig
