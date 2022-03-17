5436597a1a68e87d62b31240c45743c6767d7fc7
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-03-17 12:29:35 +0530

    Update delete-mgmt-cluster.sh

--- cluster-ops/delete-mgmt-cluster.sh
+++ cluster-ops/delete-mgmt-cluster.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 read -p 'mgmtClusterName: ' mgmtClusterName
-export CLUSTER_NAME=$mgmtClusterName
-export CONFIG_FILE=/home/ubuntu/cluster-configs/$mgmtClusterName-eks-a-cluster.yaml
+export KUBECONFIG=$mgmtClusterName/$mgmtClusterName-eks-a-cluster.kubeconfig
 cd /home/ubuntu
-eksctl anywhere delete cluster -f ${CONFIG_FILE}
-rm -rf /home/ubuntu/$CLUSTER_NAME
+eksctl anywhere delete cluster -f /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
+rm -rf /home/ubuntu/$mgmtClusterName
