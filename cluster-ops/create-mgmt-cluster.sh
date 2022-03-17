a9e8ad3cd051a7a5721064a7e403faf6c88b3d00
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-03-17 17:53:38 +0530

    Update create-mgmt-cluster.sh

--- cluster-ops/create-mgmt-cluster.sh
+++ cluster-ops/create-mgmt-cluster.sh
@@ -1,8 +1,13 @@
 #!bin/bash
 read -p 'mgmtClusterName: ' mgmtClusterName
 read -p 'staticIp: ' staticIp
+if ping -c 1 $staticIp &> /dev/null
+then
+  echo "Error., cannot contintue...Static IP conflict, address in use"
+else
 export $mgmtClusterName
 cd /home/ubuntu
 cp /home/ubuntu/eks-anywhere/cluster-samples/mgmt-eks-a-cluster-sample.yaml /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 sed -i "s/staticIp/$staticIp/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 eksctl anywhere create cluster -f /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
+fi
