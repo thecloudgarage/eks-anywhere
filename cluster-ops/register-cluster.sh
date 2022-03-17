a61ffc3c92afb7f3c89377cafe3155e2127fcb98
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-03-17 22:19:23 +0530

    Update register-cluster.sh

--- cluster-ops/register-cluster.sh
+++ cluster-ops/register-cluster.sh
@@ -7,9 +7,9 @@ cd /home/ubuntu/$workloadClusterName
 mkdir eks-connector && cd eks-connector
 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY eksctl register cluster --name $workloadClusterName --provider eks_anywhere
  --region $AWS_REGION
+CLUSTER_NAME=$workloadClusterName
 KUBECONFIG=/home/ubuntu/$workloadClusterName/$workloadClusterName-eks-a-cluster.kubeconfig
 kubectl apply -f eks-connector.yaml
-kubectl apply -f eks-connector-clusterrole.yaml
 kubectl apply -f eks-connector-clusterrole.yaml
 kubectl apply -f eks-connector-console-dashboard-full-access-group.yaml
 cd /home/ubuntu
