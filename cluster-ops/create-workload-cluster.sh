882483554446a3610250593621d27fa8e8231b3c
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-03-17 17:52:44 +0530

    Update create-workload-cluster.sh

--- cluster-ops/create-workload-cluster.sh
+++ cluster-ops/create-workload-cluster.sh
@@ -2,10 +2,15 @@
 read -p 'workloadClusterName: ' workloadClusterName
 read -p 'mgmtClusterName: ' mgmtClusterName
 read -p 'staticIp: ' staticIp
+if ping -c 1 $staticIp &> /dev/null
+then
+  echo "Error., cannot contintue...Static IP conflict, address in use"
+else
 cd /home/ubuntu/
 cp /home/ubuntu/eks-anywhere/cluster-samples/workload-eks-a-cluster-sample.yaml /home/ubuntu/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/workloadclustername/$workloadClusterName/g" $workloadClusterName-eks-a-cluster.yaml
-sed -i "s/staticIp/$staticIp/g" $workloadClusterName-eks-a-cluster.yaml 
+sed -i "s/staticIp/$staticIp/g" $workloadClusterName-eks-a-cluster.yaml
 eksctl anywhere create cluster \
   -f /home/ubuntu/$workloadClusterName-eks-a-cluster.yaml  \
   --kubeconfig /home/ubuntu/$mgmtClusterName/$mgmtClusterName-eks-a-cluster.kubeconfig
+fi
