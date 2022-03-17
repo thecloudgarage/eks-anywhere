a82a08c5b4cd895ae89081987203f7790679a7cb
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-03-17 10:01:29 +0530

    Create delete-workload-cluster.sh

---
+++ cluster-ops/delete-workload-cluster.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+read -p 'workloadClusterName: ' workloadClusterName
+read -p 'mgmtClusterName: ' mgmtClusterName
+export KUBECONFIG=$workloadClusterName/$workloadClusterName-eks-a-cluster.kubeconfig
+export MANAGEMENT_KUBECONFIG=$mgmtClusterName/$mgmtClusterName-eks-a-cluster.kubeconfig
+cd /home/ubuntu
+eksctl anywhere delete cluster $workloadClusterName --kubeconfig ${MANAGEMENT_KUBECONFIG}
+rm -rf $workloadClusterName
