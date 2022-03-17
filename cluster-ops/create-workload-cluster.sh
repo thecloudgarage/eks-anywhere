0b1b0911965f590dfef330aa564e0def2bd1812e
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-03-17 11:36:48 +0530

    Update create-workload-cluster.sh

--- cluster-ops/create-workload-cluster.sh
+++ cluster-ops/create-workload-cluster.sh
@@ -2,8 +2,9 @@
 read -p 'workloadClusterName: ' workloadClusterName
 read -p 'mgmtClusterName: ' mgmtClusterName
 cd /home/ubuntu/
-cp 
+cp /home/ubuntu/eks-anywhere/cluster-samples/workload-eks-a-cluster-sample.yaml \ 
+   /home/ubuntu/cluster-configs/$workloadClusterName-eks-a-cluster.yaml
 sed -i 's/w01/$workloadClusterName/g' 
 eksctl anywhere create cluster \
-  -f /home/ubuntu/cluster-configs/eks-a-$workloadClusterName-cluster.yaml  \
+  -f /home/ubuntu/cluster-configs/$workloadClusterName-eks-a-cluster.yaml  \
   --kubeconfig $mgmtClusterName/$mgmtClusterName-eks-a-cluster.kubeconfig
