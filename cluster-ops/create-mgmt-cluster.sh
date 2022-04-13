d0ecfec96f5412bd30d0c81281a7393ede6d44d8
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-04-13 11:23:17 +0530

    Update create-mgmt-cluster.sh

--- cluster-ops/create-mgmt-cluster.sh
+++ cluster-ops/create-mgmt-cluster.sh
@@ -9,5 +9,6 @@ export $mgmtClusterName
 cd /home/ubuntu
 cp /home/ubuntu/eks-anywhere/cluster-samples/mgmt-eks-a-cluster-sample.yaml /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 sed -i "s/staticIp/$staticIp/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
+sed -i "s/mgmt/$mgmtClusterName/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 eksctl anywhere create cluster -f /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 fi
