b9e587e781339df5f234ce551e7fc97a7f488354
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-04-24 13:13:50 +0530

    Update eksa-admin-machine-bootstrap-utils.sh

--- eksa-admin-machine/eksa-admin-machine-bootstrap-utils.sh
+++ eksa-admin-machine/eksa-admin-machine-bootstrap-utils.sh
@@ -15,7 +15,6 @@ sudo usermod -aG docker ubuntu
 sudo apt update -y
 wget https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 chmod +x install.sh
-exit
 ./install.sh
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
