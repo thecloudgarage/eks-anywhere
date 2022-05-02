e2e4be23a58aeedf4c4cd38a5da05dcfaff2473c
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-05-02 11:31:17 +0530

    Update create-mgmt-oidc-cluster.sh

--- cluster-ops/create-mgmt-oidc-cluster.sh
+++ cluster-ops/create-mgmt-oidc-cluster.sh
@@ -9,6 +9,6 @@ export $mgmtClusterName
 cd /home/ubuntu
 cp /home/ubuntu/eks-anywhere/cluster-samples/mgmt-eks-a-oidc-cluster-sample.yaml /home/ubuntu/$mgmtClusterName-eks-a-oidc-cluster.yaml
 sed -i "s/staticIp/$staticIp/g" /home/ubuntu/$mgmtClusterName-eks-a-oidc-cluster.yaml
-sed -i "s/mgmt/$mgmtClusterName-oidc/g" /home/ubuntu/$mgmtClusterName-eks-a-oidc-cluster.yaml
+sed -i "s/mgmt/$mgmtClusterName/g" /home/ubuntu/$mgmtClusterName-eks-a-oidc-cluster.yaml
 eksctl anywhere create cluster -f /home/ubuntu/$mgmtClusterName-eks-a-oidc-cluster.yaml
 fi
