1300259dd7ff8aaf23435291965d54dba174a353
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-05-02 21:19:17 +0530

    Update new-keycloak-server.sh

--- oidc/new-keycloak-server.sh
+++ oidc/new-keycloak-server.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 read -p 'fqdnOfKeycloakServer: ' fqdnOfKeycloakServer
+cp /home/ubuntu/eks-anywhere/oidc/sslcert.conf.sample /home/ubuntu/eks-anywhere/oidc/sslcert.conf
+cp /home/ubuntu/eks-anywhere/oidc/keycloak.tf.sample /home/ubuntu/eks-anywhere/oidc/keycloak.tf
 sed -i "s/fqdnOfKeycloakServer/$fqdnOfKeycloakServer/g" /home/ubuntu/eks-anywhere/oidc/sslcert.conf
 sed -i "s/fqdnOfKeycloakServer/$fqdnOfKeycloakServer/g" /home/ubuntu/eks-anywhere/oidc/keycloak.tf
 openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout tls.key -out tls.crt -config sslcert.conf -extensions 'v3_req'
