0ea4be249c1de0e689a6d215d07408c34d0d2345
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-05-04 18:22:49 +0530

    Update enable-oidc-on-clusters.sh

--- oidc/enable-oidc-on-clusters.sh
+++ oidc/enable-oidc-on-clusters.sh
@@ -3,6 +3,7 @@ HOSTS=$(kubectl get nodes --selector='node-role.kubernetes.io/master' \
     -o template \
     --template='{{range.items}}{{range.status.addresses}}{{if eq .type "ExternalIP"}}{{.address}}{{end}}{{end}} {{end}}')
 keycloakCert=$(cat /home/ubuntu/eks-anywhere/oidc/tls.crt | base64 -w 0)
+oidcConfig=$(cat /home/ubuntu/eks-anywhere/oidc/oidc-config.yaml | base64 -w 0)
 read -p 'clusterName: ' clusterName
 #read -p 'nodeIp: ' nodeIp
 for HOST in $HOSTS
@@ -14,6 +15,7 @@ ssh \
     capv@$HOST \
     "sudo echo $keycloakCert > keycloakbase64.crt && cat keycloakbase64.crt | base64 --decode > keycloak.crt \
     && sudo cp keycloak.crt /usr/local/share/ca-certificates && sudo update-ca-certificates \
-    && sudo sed -i '/tls-private-key-file/r oidc-config.yaml' \
+    && sudo echo $oidcConfig > oidcConfigbase64.yaml && cat oidcConfigbase64.yaml | base64 --decode > oidcConfig.yaml \
+    && sudo sed -i '/tls-private-key-file/r oidcConfig.yaml' \
     /etc/kubernetes/manifests/kube-apiserver.yaml"
 done
