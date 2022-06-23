94e73b6857dc8f605d77a5ed566bd65972fa519a
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-06-23 17:10:44 +0530

    Create delete-sockshop.sh

---
+++ sock-shop/delete-sockshop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+kubectl delete -f complete-demo-with-persistence.yaml
+kubectl delete -f ingress-sockshop.yaml
+kubectl delete secret sockshop -n sock-shop
+sleep 60
+kubectl delete ns sock-shop
+kubectl delete -f ingress-controller-nginx.yaml
+helm delete metallb -n metallb-system
+sleep 20
+kubectl delete ns metallb-system
+rm -rf ingress-sockshop.yaml
+rm -rf sslcert.conf
