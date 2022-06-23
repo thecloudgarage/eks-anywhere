04a2210189277d434098d9de6b1614fcd028b496
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-06-23 16:55:09 +0530

    Update test.sh

--- sock-shop/test.sh
+++ sock-shop/test.sh
@@ -20,9 +20,9 @@ sed -i "s/fqdnOfSockShopFrontEnd/$fqdnOfSockShopFrontEnd/g" ./sslcert.conf
 cp ./ingress-sockshop.yaml.sample ./ingress-sockshop.yaml
 sed -i "s/fqdnOfSockShopFrontEnd/$fqdnOfSockShopFrontEnd/g" ./ingress-sockshop.yaml
 openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout tls.key -out tls.crt -config sslcert.conf -extensions 'v3_req'
-kubectl create secret tls sockshop -n sock-shop --key tls.key --cert tls.crt
-sleep 3
 kubectl create -f namespace-sockshop.yaml
+sleep 3
+kubectl create secret tls sockshop -n sock-shop --key tls.key --cert tls.crt
 sleep 3
 kubectl create -f ingress-controller-nginx.yaml
 sleep 10
