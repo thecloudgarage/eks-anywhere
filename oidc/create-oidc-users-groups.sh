fe738c10fa437e68af0de0a51c17b282ea7391d7
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-06-26 10:23:25 +0530

    Update create-oidc-users-groups.sh

--- oidc/create-oidc-users-groups.sh
+++ oidc/create-oidc-users-groups.sh
@@ -6,6 +6,3 @@ sed -i "s/fqdnOfKeycloakServer/$fqdnOfKeycloakServer/g" $HOME/eks-anywhere/oidc/
 terraform init
 terraform plan
 terraform apply -auto-approve
-source $HOME/eks-anywhere/cluster-ops/switch-cluster.sh
-kubectl apply -f keycloak-rbac.yaml
-cd /home/ubuntu
