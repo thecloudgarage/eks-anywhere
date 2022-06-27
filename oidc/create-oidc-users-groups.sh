76c091398bc958f61d5a17dfadd9adc7f8d5acfa
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-06-27 07:20:25 +0530

    Update create-oidc-users-groups.sh

--- oidc/create-oidc-users-groups.sh
+++ oidc/create-oidc-users-groups.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 read -p 'fqdnOfKeycloakServer: ' fqdnOfKeycloakServer
+read -p 'emailDomainName: ' emailDomainName
 cd $HOME/eks-anywhere/oidc
 cp keycloak.tf.sample keycloak.tf
 sed -i "s/fqdnOfKeycloakServer/$fqdnOfKeycloakServer/g" $HOME/eks-anywhere/oidc/keycloak.tf
+sed -i "s/emailDomainNamer/$emailDomainName/g" $HOME/eks-anywhere/oidc/keycloak.tf
 terraform init
 terraform plan
 terraform apply -auto-approve
