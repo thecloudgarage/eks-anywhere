9c0551e1aacf929db2a5f607d55373b2403f65e6
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-06-27 08:46:25 +0530

    Update create-cluster-entry-in-kubeconfig.sh

--- oidc/create-cluster-entry-in-kubeconfig.sh
+++ oidc/create-cluster-entry-in-kubeconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p 'clusterName: ' clusterName
 echo "Provide the API server endpoint in the format https://172.24.165.11:6443"
-echo "Ensure that https and port number 6443 is mentioned as specififed in the above format
+echo "Ensure that https and port number 6443 is mentioned as specififed in the above format"
 echo "Only in cases e.g. eks public clusters, you can omit the port number as it provides a load-balancer URL"
 read -p 'apiServerEndpoint: ' apiServerEndpoint
 kubectl config --kubeconfig=$HOME/.kube/config set-cluster \
