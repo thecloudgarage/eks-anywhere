3c55b2370a8254981342749dc7944b62edade44c
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-06-29 18:28:36 +0530

    Update create-snapshot.sh

--- mysql/standalone/powerstore-iscsi/create-snapshot.sh
+++ mysql/standalone/powerstore-iscsi/create-snapshot.sh
@@ -2,6 +2,7 @@
 NOW=$(date "+%Y%m%d%H%M%S")
 rm -rf $HOME/eks-anywhere/mysql/standalone/powerstore-iscsi/snapshot.yaml
 cp $HOME/eks-anywhere/mysql/standalone/powerstore-iscsi/snapshot-sample.yaml \
-$HOME/eks-anywhere/mysql/standalone/powerstore-iscsi/snapshot.yaml
+        $HOME/eks-anywhere/mysql/standalone/powerstore-iscsi/snapshot.yaml
 sed -i "s/datetime/$NOW/g" $HOME/eks-anywhere/mysql/standalone/powerstore-iscsi/snapshot.yaml
 kubectl create -f $HOME/eks-anywhere/mysql/standalone/powerstore-iscsi/snapshot.yaml
+
