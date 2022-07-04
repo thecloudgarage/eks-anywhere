2fa31a348a0a563b119fa2a555dfa350d4c2ee0a
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-07-04 18:58:50 +0530

    Update create-mgmt-cluster-gitops-gitlab.sh

--- cluster-ops/create-mgmt-cluster-gitops-gitlab.sh
+++ cluster-ops/create-mgmt-cluster-gitops-gitlab.sh
@@ -41,4 +41,5 @@ sed -i "s/gitlabFQDN/$gitlabFQDN/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.
 sed -i "s/gitlabSshPort/$gitlabSshPort/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 sed -i "s/gitlabUsername/$gitlabUsername/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 sed -i "s/gitlabFluxClusterRepo/$gitlabFluxClusterRepo/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
+eksctl anywhere create cluster -f /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 fi
