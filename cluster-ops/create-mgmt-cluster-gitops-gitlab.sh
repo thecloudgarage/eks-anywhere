1fb81c281e144a1b9960a0abb532129fe37988f9
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-07-04 18:58:07 +0530

    Update create-mgmt-cluster-gitops-gitlab.sh

--- cluster-ops/create-mgmt-cluster-gitops-gitlab.sh
+++ cluster-ops/create-mgmt-cluster-gitops-gitlab.sh
@@ -5,6 +5,8 @@ read -p 'gitlabFQDN: ' gitlabFQDN
 read -p 'gitlabSshPort: ' gitlabSshPort
 read -p 'gitlabUsername: ' gitlabUsername
 read -p 'gitlabFluxClusterRepo: ' gitlabFluxClusterRepo
+export EKSA_GIT_PRIVATE_KEY=$HOME/.ssh/gitlab
+export EKSA_GIT_KNOWN_HOSTS=$HOME/.ssh/my_eksa_known_hosts
 if ping -c 1 $staticIp &> /dev/null
 then
   echo "Error., cannot contintue...Static IP conflict, address in use"
@@ -16,24 +18,27 @@ cp $HOME/eks-anywhere/cluster-samples/mgmt-eks-a-cluster-sample.yaml $HOME/$mgmt
 sed -i '/clusterNetwork:/i \
   gitOpsRef:\
     kind: FluxConfig\
-    name: $mgmtClusterName' $HOME/$mgmtClusterName-eks-a-cluster.yaml
+    name: mgmt' $HOME/$mgmtClusterName-eks-a-cluster.yaml
 #NOTE HOW WE ARE USING SED TO INSERT THE ENTIRE FLUX CONFIG AT THE END OF THE YAML FILE
 sed -i '$a\
 apiVersion: anywhere.eks.amazonaws.com/v1alpha1\
 kind: FluxConfig\
 metadata:\
-  name: $mgmtClusterName\
+  name: mgmt\
   namespace: default\
 spec:\
   branch: main\
-  clusterConfigPath: clusters/$mgmtClusterName\
+  clusterConfigPath: clusters/mgmt\
   git:\
-    repositoryUrl: ssh://git@$gitlabFQDN:$gitlabSshPort/$gitlabUsername/$gitlabFluxClusterRepo.git\
+    repositoryUrl: ssh://git@gitlabFQDN:gitlabSshPort/gitlabUsername/gitlabFluxClusterRepo.git\
     sshKeyAlgorithm: ecdsa\
   systemNamespace: flux-system\
 \
 ---' $HOME/$mgmtClusterName-eks-a-cluster.yaml
 sed -i "s/staticIp/$staticIp/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 sed -i "s/mgmt/$mgmtClusterName/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
-eksctl anywhere create cluster -f /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
+sed -i "s/gitlabFQDN/$gitlabFQDN/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
+sed -i "s/gitlabSshPort/$gitlabSshPort/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
+sed -i "s/gitlabUsername/$gitlabUsername/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
+sed -i "s/gitlabFluxClusterRepo/$gitlabFluxClusterRepo/g" /home/ubuntu/$mgmtClusterName-eks-a-cluster.yaml
 fi
