37f630c38b89582facc53887b150f54d8269035d
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-07-04 21:21:57 +0530

    Update create-workload-cluster-gitops-gitlab.sh

--- cluster-ops/create-workload-cluster-gitops-gitlab.sh
+++ cluster-ops/create-workload-cluster-gitops-gitlab.sh
@@ -7,15 +7,17 @@ read -p 'gitlabFQDN: ' gitlabFQDN
 read -p 'gitlabSshPort: ' gitlabSshPort
 read -p 'gitlabUsername: ' gitlabUsername
 read -p 'gitlabFluxClusterRepo: ' gitlabFluxClusterRepo
+export EKSA_GIT_PRIVATE_KEY=$HOME/.ssh/gitlab
+export EKSA_GIT_KNOWN_HOSTS=$HOME/.ssh/my_eksa_known_hosts
 if ping -c 1 $staticIp &> /dev/null
 then
   echo "Error., cannot contintue...Static IP conflict, address in use"
 else
 if [ -z "$mgmtClusterName" ]
 then
-cd /home/ubuntu/
-cp /home/ubuntu/eks-anywhere/cluster-samples/workload-eks-a-cluster-sample.yaml \
-        /home/ubuntu/$workloadClusterName-eks-a-cluster.yaml
+cd $HOME
+cp $HOME/eks-anywhere/cluster-samples/workload-eks-a-cluster-sample.yaml \
+        $HOME/$workloadClusterName-eks-a-cluster.yaml
 #NOTE HOW WE ARE USING SED TO INSERT THE GITOPS REF CONFIG AT THE TOP OF THE SPEC PRECEDING THE KEYWORK clusterNetwork:
 sed -i '/clusterNetwork:/i \
   gitOpsRef:\
@@ -27,14 +29,12 @@ apiVersion: anywhere.eks.amazonaws.com/v1alpha1\
 kind: FluxConfig\
 metadata:\
   name: mgmt\
-  namespace: default\
 spec:\
   branch: main\
   clusterConfigPath: clusters/mgmt\
   git:\
     repositoryUrl: ssh://git@gitlabFQDN:gitlabSshPort/gitlabUsername/gitlabFluxClusterRepo.git\
     sshKeyAlgorithm: ecdsa\
-  systemNamespace: flux-system\
 \
 ---' $HOME/$workloadClusterName-eks-a-cluster.yaml
 
@@ -46,9 +46,9 @@ sed -i "s/gitlabUsername/$gitlabUsername/g" $HOME/$workloadClusterName-eks-a-clu
 sed -i "s/gitlabFluxClusterRepo/$gitlabFluxClusterRepo/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 eksctl anywhere create cluster -f $HOME/$workloadClusterName-eks-a-cluster.yaml
 else
-cd /home/ubuntu/
-cp /home/ubuntu/eks-anywhere/cluster-samples/workload-eks-a-cluster-sample.yaml \
-        /home/ubuntu/$workloadClusterName-eks-a-cluster.yaml
+cd $HOME
+cp $HOME/eks-anywhere/cluster-samples/workload-eks-a-cluster-sample.yaml \
+        $HOME/$workloadClusterName-eks-a-cluster.yaml
 #NOTE HOW WE ARE USING SED TO INSERT THE GITOPS REF CONFIG AT THE TOP OF THE SPEC PRECEDING THE KEYWORK clusterNetwork:
 sed -i '/clusterNetwork:/i \
   gitOpsRef:\
@@ -60,14 +60,12 @@ apiVersion: anywhere.eks.amazonaws.com/v1alpha1\
 kind: FluxConfig\
 metadata:\
   name: mgmt\
-  namespace: default\
 spec:\
   branch: main\
   clusterConfigPath: clusters/mgmt\
   git:\
     repositoryUrl: ssh://git@gitlabFQDN:gitlabSshPort/gitlabUsername/gitlabFluxClusterRepo.git\
     sshKeyAlgorithm: ecdsa\
-  systemNamespace: flux-system\
 \
 ---' $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/workloadclustername/$workloadClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
@@ -77,7 +75,7 @@ sed -i "s/gitlabSshPort/$gitlabSshPort/g" $HOME/$workloadClusterName-eks-a-clust
 sed -i "s/gitlabUsername/$gitlabUsername/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/gitlabFluxClusterRepo/$gitlabFluxClusterRepo/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 eksctl anywhere create cluster \
-  -f /home/ubuntu/$workloadClusterName-eks-a-cluster.yaml  \
+  -f $HOME/$workloadClusterName-eks-a-cluster.yaml  \
   --kubeconfig $HOME/$mgmtClusterName/$mgmtClusterName-eks-a-cluster.kubeconfig
 fi
 fi
