039dbdd50f9d919087c42f8c2a396cb81270b991
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-07-12 18:14:42 +0530

    Create ubuntu-2004.tf

---
+++ eksa-admin-machine/terraform/ubuntu-2004.tf
@@ -0,0 +1,100 @@
+# Basic configuration withour variables
+
+# Define authentification configuration
+provider "vsphere" {
+  # If you use a domain set your login like this "MyDomain\\MyUser"
+  user           = "ambar@vsphere.local"
+  password       = "password"
+  vsphere_server = "vc.iac.ssc"
+
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+#### RETRIEVE DATA INFORMATION ON VCENTER ####
+
+data "vsphere_datacenter" "dc" {
+  name = "IAC-SSC"
+}
+
+data "vsphere_resource_pool" "pool" {
+  # If you haven't resource pool, put "Resources" after cluster name
+  name          = "Test"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Retrieve datastore information on vsphere
+data "vsphere_datastore" "datastore" {
+  name          = "CommonDS"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Retrieve network information on vsphere
+data "vsphere_network" "network" {
+  name          = "iac_pg"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# NOTE: YOU WILL NEED AN EXISTING TEMPLATE IN THE TEMPLATES FOLDER. Retrieve template information on vsphere
+data "vsphere_virtual_machine" "template" {
+  name          = "ubuntu-2004-desktop"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+#### VM CREATION ####
+
+# Set vm parameters
+resource "vsphere_virtual_machine" "vm-one" {
+  name             = "ubuntu"
+  folder           = "test-eks-anywhere"
+  num_cpus         = 4
+  memory           = 16384
+  datastore_id     = data.vsphere_datastore.datastore.id
+#  host_system_id   = data.vsphere_host.host.id
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+
+  # Set network parameters
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  # Use a predefined vmware template has main disk
+  disk {
+    label       = "disk0"
+    size        = "100"
+    thin_provisioned = "false"
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "ubuntu"
+        domain    = "iac.ssc"
+      }
+
+      network_interface {
+        ipv4_address    = "172.24.165.50"
+        ipv4_netmask    = 22
+        dns_server_list = ["172.24.164.10"]
+      }
+
+      ipv4_gateway = "172.24.164.1"
+    }
+  }
+
+  # Execute script on remote vm after this creation
+  # Execute script on remote vm after this creation
+  provisioner "remote-exec" {
+    script = "scripts/example-script.sh"
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      password = "password"
+      host     = "172.24.165.50"
+    }
+  }
+}
