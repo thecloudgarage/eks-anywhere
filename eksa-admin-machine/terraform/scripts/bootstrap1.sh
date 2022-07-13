27d7074e77a101ee4207125c6c040d123d666fbd
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-07-13 13:49:25 +0530

    Create bootstrap1.sh

---
+++ eksa-admin-machine/terraform/scripts/bootstrap1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt-get install build-essential procps curl file git zip unzip sshpass jq -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get update -y
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo usermod -aG docker ubuntu
+echo "done"
