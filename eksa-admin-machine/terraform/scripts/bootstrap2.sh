0ce842a4e90012f9da8d56bf91c6ce5ca72b5690
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-07-19 19:46:33 +0530

    Delete bootstrap2.sh

--- eksa-admin-machine/terraform/scripts/bootstrap2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-chmod +x install.sh
-./install.sh
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "done"
