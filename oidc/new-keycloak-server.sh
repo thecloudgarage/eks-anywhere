4fd661015892e50ab9b85f9beeb0b1d296c6c04f
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-07-20 13:19:44 +0530

    Update new-keycloak-server.sh

--- oidc/new-keycloak-server.sh
+++ oidc/new-keycloak-server.sh
@@ -3,6 +3,7 @@ read -p 'fqdnOfKeycloakServer: ' fqdnOfKeycloakServer
 cp $HOME/eks-anywhere/oidc/sslcert.conf.sample $HOME/eks-anywhere/oidc/sslcert.conf
 cp $HOME/eks-anywhere/oidc/keycloak.tf.sample $HOME/eks-anywhere/oidc/keycloak.tf
 sed -i "s/fqdnOfKeycloakServer/$fqdnOfKeycloakServer/g" $HOME/eks-anywhere/oidc/sslcert.conf
-sed -i "s/fqdnOfKeycloakServer/$fqdnOfKeycloakServer/g" $HOME/eks-anywhere/oidc/keycloak.tf
+sed -i "s/fqdnOfKeycloakServer/$fqdnOfKeycloakServer/g" $HOME/eks-anywhere/oidc/keycloak.tf.sample
 openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout tls.key -out tls.crt -config sslcert.conf -extensions 'v3_req'
+cp $HOME/eks-anywhere/oidc/tls.crt $HOME/eks-anywhere/oidc/keycloak.pem
 docker-compose up -d
