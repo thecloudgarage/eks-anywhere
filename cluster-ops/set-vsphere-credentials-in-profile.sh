6cc2bb67127625a09feb4529bb5aa8dcbf6cdc6f
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-07-27 21:30:30 +0530

    Update set-vsphere-credentials-in-profile.sh

--- cluster-ops/set-vsphere-credentials-in-profile.sh
+++ cluster-ops/set-vsphere-credentials-in-profile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 read -sp 'vSphereUserName: ' vSphereUserName
 read -sp 'vSpherePassword: ' vSpherePassword
+echo -e "\n"
 sed -i '/^EKSA_VSPHERE_/d' /home/ubuntu/.profile
 echo "EKSA_VSPHERE_USERNAME=$vSphereUserName; export EKSA_VSPHERE_USERNAME" >> ~/.profile
 echo "EKSA_VSPHERE_PASSWORD=$vSpherePassword; export EKSA_VSPHERE_PASSWORD" >> ~/.profile
