c12bdd7a2fb361a8c5d93d4e02f9fef4962196e3
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-08-01 19:10:32 +0530

    Update deploy-sockshop.sh

--- sock-shop/deploy-sockshop.sh
+++ sock-shop/deploy-sockshop.sh
@@ -1,16 +1,17 @@
 #!bin/bash
 read -p 'fqdnOfSockShopFrontEnd: ' fqdnOfSockShopFrontEnd
+cd $HOME/eks-anywhere/sock-shop/
 cp ./sslcert.conf.sample ./sslcert.conf
 sed -i "s/fqdnOfSockShopFrontEnd/$fqdnOfSockShopFrontEnd/g" ./sslcert.conf
 cp ./ingress-sockshop.yaml.sample ./ingress-sockshop.yaml
 sed -i "s/fqdnOfSockShopFrontEnd/$fqdnOfSockShopFrontEnd/g" ./ingress-sockshop.yaml
 openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout tls.key -out tls.crt -config sslcert.conf -extensions 'v3_req'
-kubectl create -f namespace-sockshop.yaml
+kubectl create -f $HOME/eks-anywhere/sock-shop/namespace-sockshop.yaml
 sleep 3
 kubectl create secret tls sockshop-tls -n sock-shop --key tls.key --cert tls.crt
 sleep 3
-kubectl create -f ingress-controller-nginx.yaml
+kubectl create -f $HOME/eks-anywhere/sock-shop/ingress-controller-nginx.yaml
 sleep 10
-kubectl create -f complete-demo-with-persistence.yaml
+kubectl create -f $HOME/eks-anywhere/sock-shop/complete-demo-with-persistence.yaml
 sleep 60
-kubectl create -f ingress-sockshop.yaml
+kubectl create -f $HOME/eks-anywhere/sock-shop/ingress-sockshop.yaml
