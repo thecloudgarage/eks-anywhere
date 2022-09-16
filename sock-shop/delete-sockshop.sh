0ba91e742473a8a6e140cbebaae7086c956654ce
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-09-16 23:11:45 +0530

    Update delete-sockshop.sh

--- sock-shop/delete-sockshop.sh
+++ sock-shop/delete-sockshop.sh
@@ -5,9 +5,7 @@ kubectl delete secret sockshop -n sock-shop
 sleep 60
 kubectl delete ns sock-shop
 kubectl delete -f ingress-controller-nginx.yaml
-helm delete metallb -n metallb-system
 sleep 20
-kubectl delete ns metallb-system
 rm -rf ingress-sockshop.yaml
 rm -rf tls*
 rm -rf sslcert.conf
