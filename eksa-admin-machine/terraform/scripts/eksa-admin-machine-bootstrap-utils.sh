be23f36b03efd93d8d99ef4d8f5bb76960ef5af8
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-09-19 10:36:52 +0530

    Create eksa-admin-machine-bootstrap-utils.sh

---
+++ eksa-admin-machine/terraform/scripts/eksa-admin-machine-bootstrap-utils.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt-get install build-essential procps curl file git zip unzip sshpass jq -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get update -y
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo usermod -aG docker ubuntu
+sudo apt update -y
+# Uncomment below if challenged with Homebrew installation
+#git config --global --unset http.proxy 
+#git config --global --unset https.proxy
+wget https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+chmod +x install.sh
+#NOTE HOW WE ARE PASSING AN ENTER FOR THE INTERACTIVE PROMPT THAT THE INSTALL SCRIPT GENERATES TO CONFIRM FOR INSTALLATION
+sudo echo -ne '\n' | ./install.sh
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo useradd -m -p ubuntu image-builder
+sudo usermod -aG sudo image-builder
+echo "done"
