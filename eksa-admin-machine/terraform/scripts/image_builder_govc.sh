98a6b9dfad9345d8ff98ea6c23f0954e1cab9bfe
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-09-20 23:43:13 +0530

    Update image_builder_govc.sh

--- eksa-admin-machine/terraform/scripts/image_builder_govc.sh
+++ eksa-admin-machine/terraform/scripts/image_builder_govc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p /home/image-builder/.ssh
+sudo mkdir -p /tmp/eks-image-builder-cni
+sudo chown -R image-builder:image-builder /tmp/eks-image-builder-cni
 sudo apt update -y
 sudo snap install yq
 sudo apt install jq unzip make ansible -y
