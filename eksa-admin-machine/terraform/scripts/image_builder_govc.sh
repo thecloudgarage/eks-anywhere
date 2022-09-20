38282ebfa87a8730b624b6543a37f675b310b560
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-09-20 10:39:44 +0530

    Update image_builder_govc.sh

--- eksa-admin-machine/terraform/scripts/image_builder_govc.sh
+++ eksa-admin-machine/terraform/scripts/image_builder_govc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt install jq unzip make ansible -y
-sudo apt install python3-pip -y
 sudo snap install yq
+sudo apt install python3-pip -y
+sudo apt install jq unzip make ansible -y
 mkdir -p /home/image-builder/.ssh
 cd /tmp
 sudo wget https://anywhere-assets.eks.amazonaws.com/releases/bundles/15/artifacts/image-builder/0.1.0/image-builder-linux-amd64.tar.gz
