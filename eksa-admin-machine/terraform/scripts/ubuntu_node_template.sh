1ff18145d52de771a4ad30c4f88fea03045b5e5d
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-09-21 11:12:57 +0530

    Update ubuntu_node_template.sh

--- eksa-admin-machine/terraform/scripts/ubuntu_node_template.sh
+++ eksa-admin-machine/terraform/scripts/ubuntu_node_template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-vsphere_library_name=eks-anywhere-template-$RANDOM
-govc library.create "$vsphere_library_name"
-sed -i "s/vsphere_library_name/$vsphere_library_name/g" $HOME/vsphere-connection.json
+vsphere_content_library=eks-anywhere-template-$RANDOM
+govc library.create "$vsphere_content_library"
+sed -i "s/vsphere_library_name/$vsphere_content_library/g" $HOME/vsphere-connection.json
 image-builder build --os ubuntu --hypervisor vsphere --release-channel 1-21 --vsphere-config $HOME/vsphere-connection.json
 sudo -Hiu ubuntu eksctl anywhere download artifacts
 sudo -Hiu ubuntu tar -xzf eks-anywhere-downloads.tar.gz
@@ -9,8 +9,8 @@ export eksd_release_tag=$(file=$(sudo -Hiu ubuntu ls eks-anywhere-downloads/1.21
 echo $file| rev | awk -v FS='.' '{print $2}' | rev)
 export vsphere_templates_folder_full_path=/vsphere_datacenter/vm/vsphere_templates_folder
 cp ubuntu.ova ubuntu-2004-kube-v1.21.14.ova
-govc library.import $vsphere_library_name /home/image-builder/ubuntu-2004-kube-v1.21.14.ova
-govc library.deploy -pool Test -folder $vsphere_templates_folder_full_path /$vsphere_library_name/ubuntu-2004-kube-v1.21.14 ubuntu-2004-kube-v1.21.14
+govc library.import $vsphere_content_library /home/image-builder/ubuntu-2004-kube-v1.21.14.ova
+govc library.deploy -pool Test -folder $vsphere_templates_folder_full_path /$vsphere_content_library/ubuntu-2004-kube-v1.21.14 ubuntu-2004-kube-v1.21.14
 govc snapshot.create -vm ubuntu-2004-kube-v1.21.14 root
 govc vm.markastemplate ubuntu-2004-kube-v1.21.14
 govc tags.create -c os os:ubuntu
