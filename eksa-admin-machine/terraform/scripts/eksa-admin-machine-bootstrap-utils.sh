e7d99da5a8c3506c970f15ff42fc4868f4f17a78
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-09-21 11:01:52 +0530

    Update eksa-admin-machine-bootstrap-utils.sh

--- eksa-admin-machine/terraform/scripts/eksa-admin-machine-bootstrap-utils.sh
+++ eksa-admin-machine/terraform/scripts/eksa-admin-machine-bootstrap-utils.sh
@@ -2,8 +2,6 @@
 sudo adduser --gecos "" --disabled-password image-builder
 sudo chpasswd <<<"image-builder:virtual_machine_root_password"
 sudo usermod -aG sudo image-builder
-sudo mkdir -p /tmp/eks-image-builder-cni
-sudo chown -R image-builder:image-builder /tmp/eks-image-builder-cni
 touch useradded.txt
 sudo apt-get install build-essential procps curl file git zip unzip sshpass jq -y
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
