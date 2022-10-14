bbb5554c7c8d872df02ef142b6fdbf8ffa67b4a3
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-10-14 07:10:04 +0530

    Update set-vsphere-credentials-in-profile.sh

--- cluster-ops/set-vsphere-credentials-in-profile.sh
+++ cluster-ops/set-vsphere-credentials-in-profile.sh
@@ -3,5 +3,9 @@ read -sp 'vSphereUserName: ' vSphereUserName
 read -sp 'vSpherePassword: ' vSpherePassword
 echo -e "\n"
 sed -i '/^EKSA_VSPHERE_/d' /home/ubuntu/.profile
-echo "EKSA_VSPHERE_USERNAME=$vSphereUserName; export EKSA_VSPHERE_USERNAME" >> ~/.profile
-echo "EKSA_VSPHERE_PASSWORD=$vSpherePassword; export EKSA_VSPHERE_PASSWORD" >> ~/.profile
+#echo "EKSA_VSPHERE_USERNAME=$vSphereUserName; export EKSA_VSPHERE_USERNAME" >> ~/.profile
+#echo "EKSA_VSPHERE_PASSWORD=$vSpherePassword; export EKSA_VSPHERE_PASSWORD" >> ~/.profile
+#EKS ANYWHERE DOCUMENTATION suggests using single quotes in exported variables for vsphere logins
+#The below escaped single quotes will be presented along with username and password in the env output
+echo "EKSA_VSPHERE_USERNAME=\'$vSphereUserName\'; export EKSA_VSPHERE_USERNAME" >> ~/.profile
+echo "EKSA_VSPHERE_PASSWORD=\'$vSpherePassword\'; export EKSA_VSPHERE_PASSWORD" >> ~/.profile
