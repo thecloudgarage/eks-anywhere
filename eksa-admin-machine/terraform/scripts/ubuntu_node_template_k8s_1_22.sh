2cd5ef1a7377480638f3b1d05f1ab87c67e7624f
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-11-08 18:02:37 +0530

    Update ubuntu_node_template_k8s_1_22.sh

--- eksa-admin-machine/terraform/scripts/ubuntu_node_template_k8s_1_22.sh
+++ eksa-admin-machine/terraform/scripts/ubuntu_node_template_k8s_1_22.sh
@@ -3,10 +3,13 @@ read -p 'vSphere Datacenter name: ' vsphere_datacenter
 read -p 'vSphere Default Templates folder, e.g. Templates: ' vsphere_templates_folder
 read -p 'Final name for EKS-A ubuntu node template, e.g. ubuntu-2004-kube-v1.22: ' ubuntu_template_name
 read -p 'vSphere Resource pool name, e.g. Test: ' vsphere_resource_pool
+sudo -Hiu ubuntu rm -rf eks-anywhere-downloads
+sudo -Hiu ubuntu rm -rf eks-anywhere-downloads.tar.gz
 sudo -Hiu ubuntu eksctl anywhere download artifacts
 sudo -Hiu ubuntu tar -xzf eks-anywhere-downloads.tar.gz
-export eksd_release_tag=$(file=$(sudo -Hiu ubuntu ls eks-anywhere-downloads/1.22/eks-distro/ | grep kubernetes) | \
-echo $file| rev | awk -v FS='.' '{print $2}' | rev)
+file=$(sudo -Hiu ubuntu ls /home/ubuntu/eks-anywhere-downloads/1.22/eks-distro/ | grep kubernetes)
+export eksd_release_tag=$(echo $file | rev | awk -v FS='.' '{print $2}' | rev)
+echo $eksd_release_tag
 vsphere_content_library=eks-anywhere-template-$RANDOM
 govc library.create "$vsphere_content_library"
 sed -i "s/vsphere_content_library/$vsphere_content_library/g" $HOME/vsphere-connection.json
@@ -14,7 +17,8 @@ image-builder build --os ubuntu --hypervisor vsphere --release-channel 1-22 --vs
 export vsphere_templates_folder_full_path=/$vsphere_datacenter/vm/$vsphere_templates_folder
 mv $HOME/ubuntu.ova $HOME/$ubuntu_template_name.ova
 govc library.import $vsphere_content_library $HOME/$ubuntu_template_name.ova
-govc library.deploy -pool $vsphere_resource_pool -folder $vsphere_templates_folder_full_path /$vsphere_content_library/$ubuntu_template_name $ubuntu_template_name
+govc library.deploy -pool $vsphere_resource_pool -folder $vsphere_templates_folder_full_path /$vsphere_content_library/$ubuntu_template_name $ubuntu_template
+_name
 govc snapshot.create -vm $ubuntu_template_name root
 govc vm.markastemplate $ubuntu_template_name
 govc tags.create -c os os:ubuntu 2> /dev/null
