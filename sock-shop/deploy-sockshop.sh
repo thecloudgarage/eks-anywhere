89ef451f44257d6f0c9df7cbbf363f29d2027687
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-11-19 08:56:58 +0530

    Update deploy-sockshop.sh

--- sock-shop/deploy-sockshop.sh
+++ sock-shop/deploy-sockshop.sh
@@ -10,9 +10,9 @@ kubectl create -f $HOME/eks-anywhere/sock-shop/namespace-sockshop.yaml
 sleep 3
 kubectl create secret tls sockshop-tls -n sock-shop --key tls.key --cert tls.crt
 sleep 3
-kubectl create -f $HOME/eks-anywhere/sock-shop/ingress-controller-nginx.yaml
+kubectl create -f $HOME/eks-anywhere/ingress-controllers/nginx-ingress-controller.yaml
 sleep 120
-kubectl create -f $HOME/eks-anywhere/sock-shop/ingressclassnginx.yaml
+kubectl create -f $HOME/eks-anywhere/ingress-controllers/nginx-ingress-class.yaml
 kubectl create -f $HOME/eks-anywhere/sock-shop/complete-demo-with-persistence.yaml
 sleep 60
 kubectl create -f $HOME/eks-anywhere/sock-shop/ingress-sockshop.yaml
