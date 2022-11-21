04bb8c2dc0a11b95e5e538e97a0c437b085562d1
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-11-21 22:53:37 +0530

    Update create-eksa-cluster.sh

--- cluster-ops/create-eksa-cluster.sh
+++ cluster-ops/create-eksa-cluster.sh
@@ -8,6 +8,7 @@ echo -e "${RED}#################################################################
 read -p 'Workload cluster name: ' workloadClusterName
 read -p 'Management cluster name: ' mgmtClusterName
 read -p 'staticIp for API server High Availability: ' apiServerIpAddress
+read -p 'Kubernetes version 1.21, 1.22, v1.23, etc.: ' kubernetesVersion
 if ping -c 1 $staticIp &> /dev/null
 then
   echo "Error., cannot contintue...Static IP conflict, address in use"
@@ -20,6 +21,7 @@ cp $HOME/eks-anywhere/cluster-samples/cluster-sample.yaml \
 sed -i "s/workload-cluster-name/$workloadClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/management-cluster-name/$mgmtClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/api-server-ip/$apiServerIpAddress/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
+sed -i "s/1.21/$kubernetesVersion/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 eksctl anywhere create cluster -f $HOME/$workloadClusterName-eks-a-cluster.yaml
 else
 cd $HOME
@@ -28,6 +30,7 @@ cp $HOME/eks-anywhere/cluster-samples/cluster-sample.yaml \
 sed -i "s/workload-cluster-name/$workloadClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/management-cluster-name/$mgmtClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/api-server-ip/$apiServerIpAddress/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
+sed -i "s/1.21/$kubernetesVersion/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 eksctl anywhere create cluster \
   -f $HOME/$workloadClusterName-eks-a-cluster.yaml  \
   --kubeconfig $HOME/$mgmtClusterName/$mgmtClusterName-eks-a-cluster.kubeconfig
