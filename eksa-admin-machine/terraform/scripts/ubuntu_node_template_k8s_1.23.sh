26b7b9f44a963eb5017a67184495eed4c53738df
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-12-12 12:19:57 +0530

    Create ubuntu_node_template_k8s_1.23.sh

---
+++ eksa-admin-machine/terraform/scripts/ubuntu_node_template_k8s_1.23.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+read -p 'vSphere Datacenter name: ' vsphere_datacenter
+read -p 'vSphere Default Templates folder, e.g. Templates: ' vsphere_templates_folder
+read -p 'Final name for EKS-A ubuntu node template, e.g. ubuntu-2004-kube-v1.23: ' ubuntu_template_name
+read -p 'vSphere Resource pool name, e.g. Test: ' vsphere_resource_pool
+sudo -Hiu ubuntu rm -rf eks-anywhere-downloads
+sudo -Hiu ubuntu rm -rf eks-anywhere-downloads.tar.gz
+sudo -Hiu ubuntu eksctl anywhere download artifacts
+sudo -Hiu ubuntu tar -xzf eks-anywhere-downloads.tar.gz
+file=$(sudo -Hiu ubuntu ls /home/ubuntu/eks-anywhere-downloads/1.23/eks-distro/ | grep kubernetes)
+export eksd_release_tag=$(echo $file | rev | awk -v FS='.' '{print $2}' | rev)
+echo $eksd_release_tag
+vsphere_content_library=eks-anywhere-template-$RANDOM
+govc library.create "$vsphere_content_library"
+sed -i "s/vsphere_content_library/$vsphere_content_library/g" $HOME/vsphere-connection.json
+image-builder build --os ubuntu --hypervisor vsphere --release-channel 1-23 --vsphere-config $HOME/vsphere-connection.json
+export vsphere_templates_folder_full_path=/$vsphere_datacenter/vm/$vsphere_templates_folder
+mv $HOME/ubuntu.ova $HOME/$ubuntu_template_name.ova
+govc library.import $vsphere_content_library $HOME/$ubuntu_template_name.ova
+govc library.deploy -pool $vsphere_resource_pool -folder $vsphere_templates_folder_full_path /$vsphere_content_library/$ubuntu_template_name $ubuntu_template
+_name
+govc snapshot.create -vm $ubuntu_template_name root
+govc vm.markastemplate $ubuntu_template_name
+govc tags.create -c os os:ubuntu 2> /dev/null
+govc tags.category.create -t VirtualMachine eksdRelease 2> /dev/null
+govc tags.create -c eksdRelease eksdRelease:$eksd_release_tag 2> /dev/null
+govc tags.attach os:ubuntu $vsphere_templates_folder_full_path/$ubuntu_template_name
+govc tags.attach eksdRelease:$eksd_release_tag $vsphere_templates_folder_full_path/$ubuntu_template_name
+govc library.rm "$vsphere_content_library"
+sed -i "s/$vsphere_content_library/vsphere_content_library/g" $HOME/vsphere-connection.json
+echo "$ubuntu_template_name template is created successfully and is located in $vsphere_templates_folder"
