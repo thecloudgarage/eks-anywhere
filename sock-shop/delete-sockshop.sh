38ffb669b90df42abf723c75535b4caaa45e4528
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-12-16 11:16:40 +0530

    Update delete-sockshop.sh

--- sock-shop/delete-sockshop.sh
+++ sock-shop/delete-sockshop.sh
@@ -4,8 +4,8 @@ kubectl delete -f $HOME/eks-anywhere/sock-shop/ingress-sockshop.yaml
 kubectl delete secret sockshop -n sock-shop
 sleep 60
 kubectl delete ns sock-shop
-kubectl delete -f $HOME/eks-anywhere/nginx-ingress-class.yaml
-kubectl delete -f $HOME/eks-anywhere/nginx-ingress-controller.yaml
+kubectl delete -f $HOME/eks-anywhere/ingress-controllers/nginx-ingress-class.yaml
+kubectl delete -f $HOME/eks-anywhere/ingress-controllers/nginx-ingress-controller.yaml
 sleep 20
 rm -rf $HOME/eks-anywhere/sock-shop/ingress-sockshop.yaml
 rm -rf tls*
