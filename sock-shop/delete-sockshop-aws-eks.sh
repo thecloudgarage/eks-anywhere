d0dd43e83aafad039e724273fcb7311c07648cd7
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-12-16 11:19:57 +0530

    Create delete-sockshop-aws-eks.sh

---
+++ sock-shop/delete-sockshop-aws-eks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+kubectl delete -f $HOME/eks-anywhere/sock-shop/complete-demo-with-persistence.yaml
+kubectl delete -f $HOME/eks-anywhere/sock-shop/ingress-sockshop.yaml
+kubectl delete secret sockshop -n sock-shop
+sleep 60
+kubectl delete ns sock-shop
+kubectl delete -f $HOME/eks-anywhere/ingress-controllers/nginx-ingress-class.yaml
+kubectl delete -f $HOME/eks-anywhere/ingress-controllers/nginx-ingress-controller-eks-nlb.yaml
+sleep 20
+rm -rf $HOME/eks-anywhere/sock-shop/ingress-sockshop.yaml
+rm -rf tls*
+rm -rf sslcert.conf
