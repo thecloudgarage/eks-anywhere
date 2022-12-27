5a5e8ba28b3fc45ca8e59ad1a7baad644d37950a
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-12-27 10:52:40 +0530

    Create install-powerscale-csi.sh

---
+++ powerscale/install-powerscale-csi.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+echo "Enter EKSA Cluster Name on which CSI driver needs to be installed"
+read -p 'clusterName: ' clusterName
+echo "Enter PowerScale CSI release version, e.g. 2.2.0, 2.3.0, 2.4.0, 2.5.0"
+read -p 'csiReleaseNumber: ' csiReleaseNumber
+echo "Enter PowerScale Cluster Name"
+read -p 'powerScaleClusterName: ' powerScaleClusterName
+echo "Enter IP or FQDN of the PowerStore array"
+read -p 'ipOrFqdnOfPowerStoreArray: ' ipOrFqdnOfPowerScaleCluster
+echo "Enter Global Id of the PowerStore Array"
+read -p 'userNameOfPowerStoreArray: ' userNameOfPowerScaleCluster
+echo "Enter password of the PowerStore Array"
+read -sp 'passwordOfPowerStoreArray: ' passwordOfPowerScaleCluster
+echo -e "\n"
+#SET KUBECTL CONTEXT
+printf "$clusterName\n" | source $HOME/eks-anywhere/cluster-ops/switch-cluster.sh
+source $HOME/.profile
+#CLONE THE POWERSCALE CSI REPO
+mkdir -p $HOME/$clusterName
+cd $HOME/$clusterName
+git clone -b v$csiReleaseNumber https://github.com/dell/csi-powerscale.git
+cd $HOME/$clusterName/csi-powerscale
+#EXPORT EKS DISTRO VERSION FOR CSI VERIFICATION
+eksdistroversion=$(kubectl get nodes -o=jsonpath='{.items[0].status.nodeInfo.kubeletVersion}')
+sed -i "s/>= 1.21.0 < 1.26.0/$eksdistroversion/g" helm/csi-powerscale/Chart.yaml
+cd $HOME/$clusterName/csi-powerscale
+#INSTALL EXTERNAL SNAPSHOTTER
+git clone https://github.com/kubernetes-csi/external-snapshotter/
+cd ./external-snapshotter
+git checkout release-5.0
+kubectl kustomize client/config/crd | kubectl create -f -
+kubectl -n kube-system kustomize deploy/kubernetes/snapshot-controller | kubectl create -f -
+#PREPARE FOR POWERSCALE CSI INSTALLATION
+kubectl create namespace csi-powerscale
+cd $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer
+cp $HOME/eks-anywhere/csi-powerscale/powerscale-creds.yaml powerscale-creds.yaml
+cp $HOME/eks-anywhere/csi-powerscale/emptysecret.yaml emptysecret.yaml
+#BUILD CREDS FILE FOR POWERSCALE CSI
+sed -i "s/powerscale_cluster_name/$powerScaleClusterName/g" $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer/powerscale-creds.yaml
+sed -i "s/powerscale_username/$userNameOfPowerScaleCluster/g" $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer/powerscale-creds.yaml
+sed -i "s/powerscale_password/$passwordOfPowerScaleCluster/g" $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer/powerscale-creds.yaml
+sed -i "s/powerscale_endpoint/$ipOrFqdnOfPowerScaleCluster/g" $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer/powerscale-creds.yaml
+#CREATE SECRETS FOR POWERSCALE CSI
+kubectl create secret generic isilon-creds -n isilon --from-file=config=powerscale-creds.yaml -o yaml --dry-run=client | kubectl apply -f -
+kubectl create -f $HOME/$clusterName/csi-powerscale/samples/secret/emptysecret.yaml
+#BUILD SETTINGS FILE FOR POWERSCALE CSI
+cd $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer/
+cp $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer/values.yaml my-powerscale-settings.yaml
+sed -i "s/volumeNamePrefix: k8s/volumeNamePrefix: $clusterName-vol/g" my-powerscale-settings.yaml
+sed -i "s/snapNamePrefix: snapshot/$clusterName-snap/g" my-powerscale-settings.yaml
+sed -i 's/isiAuthType: 0/isiAuthType: 1/g' my-powerscale-settings.yaml
+#INSTALL POWERSCALE CSI
+cd $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer
+./csi-install.sh --namespace csi-powerscale --values ./my-powerscale-settings.yaml --skip-verify --skip-verify-node
+#CREATE STORAGE CLASS FOR POWERSCALE CSI
+cd $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer
+cp $HOME/$clusterName/csi-powerscale/samples/storageclass/isilon.yaml ./powerscale-sc.yaml
+kubectl create -f $HOME/$clusterName/csi-powerscale/dell-csi-helm-installer/powerscale-sc.yaml
