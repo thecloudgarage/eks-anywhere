d077fbddaa9e69a2b88d4735d2e4ce59add9ad21
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-12-29 13:32:04 +0530

    Update create-snapshot.sh

--- busybox/powerscale-rwx/create-snapshot.sh
+++ busybox/powerscale-rwx/create-snapshot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NOW=$(date "+%Y%m%d%H%M%S")
-rm -rf $HOME/eks-anywhere/busybox/powerscale/snapshot.yaml
-cp $HOME/eks-anywhere/busybox/powerscale/snapshot-sample.yaml \
-        $HOME/eks-anywhere/busybox/powerscale/snapshot.yaml
-sed -i "s/datetime/$NOW/g" $HOME/eks-anywhere/busybox/powerscale/snapshot.yaml
-kubectl create -f $HOME/eks-anywhere/busybox/powerscale/snapshot.yaml
+rm -rf $HOME/eks-anywhere/busybox/powerscale-rwx/snapshot.yaml
+cp $HOME/eks-anywhere/busybox/powerscale-rwx/snapshot-sample.yaml \
+        $HOME/eks-anywhere/busybox/powerscale-rwx/snapshot.yaml
+sed -i "s/datetime/$NOW/g" $HOME/eks-anywhere/busybox/powerscale-rwx/snapshot.yaml
+kubectl create -f $HOME/eks-anywhere/busybox/powerscale-rwx/snapshot.yaml
