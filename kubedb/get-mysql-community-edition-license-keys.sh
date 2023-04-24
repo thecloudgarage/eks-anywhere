2558539f17dddaae1a007255cb0b71ad4eb9b004
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-04-24 17:48:20 +0530

    Update and rename get-license-keys.sh to get-mysql-community-edition-license-keys.sh

--- kubedb/get-mysql-community-edition-license-keys.sh
+++ kubedb/get-mysql-community-edition-license-keys.sh
@@ -8,4 +8,4 @@ read -p 'emailAddress: ' emailAddress
 clusterId=$(kubectl get ns kube-system -o=jsonpath='{.metadata.uid}')
 curl -X POST \
 -d "name=$name&email=$emailAddress&product=kubedb-community&cluster=$clusterId&tos=true&token=$kubeDbApiToken" \
-https://license-issuer.appscode.com/issue-license > $clusterId-kubedb-license-key.txt
+https://license-issuer.appscode.com/issue-license > $CLUSTER_NAME/$CLUSTER_NAME-mysql-kubedb-license-key.txt
