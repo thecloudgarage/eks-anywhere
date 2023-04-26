f5a49e0586d3571001b87f90d08bd77b3168df6d
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-04-26 10:39:57 +0530

    Update get-mysql-community-edition-license-keys.sh

--- kubedb/get-mysql-community-edition-license-keys.sh
+++ kubedb/get-mysql-community-edition-license-keys.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Enter your Kube DB API Token"
 read -sp 'kubeDbApiToken: ' kubeDbApiToken
+echo "Enter your EKS ANYWHERE name"
+read -p 'CLUSTER_NAME: ' CLUSTER_NAME
 echo "Enter your name"
 read -p 'name: ' name
 echo "Enter your registered Email with KubeDB"
@@ -8,4 +10,4 @@ read -p 'emailAddress: ' emailAddress
 clusterId=$(kubectl get ns kube-system -o=jsonpath='{.metadata.uid}')
 curl -X POST \
 -d "name=$name&email=$emailAddress&product=kubedb-community&cluster=$clusterId&tos=true&token=$kubeDbApiToken" \
-https://license-issuer.appscode.com/issue-license > $CLUSTER_NAME/$CLUSTER_NAME-mysql-kubedb-license-key.txt
+https://license-issuer.appscode.com/issue-license > $HOME/$CLUSTER_NAME/$CLUSTER_NAME-kubedb-license-key.txt
