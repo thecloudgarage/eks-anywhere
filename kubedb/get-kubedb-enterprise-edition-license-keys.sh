133c00ea2ad3d11151bf0ec4f2633bd9fe2db9d3
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-04-26 19:01:39 +0530

    Create get-kubedb-enterprise-edition-license-keys.sh

---
+++ kubedb/get-kubedb-enterprise-edition-license-keys.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter your Kube DB API Token"
+read -sp 'kubeDbApiToken: ' kubeDbApiToken
+echo "Enter your EKS ANYWHERE name"
+read -p 'CLUSTER_NAME: ' CLUSTER_NAME
+echo "Enter your name"
+read -p 'name: ' name
+echo "Enter your registered Email with KubeDB"
+read -p 'emailAddress: ' emailAddress
+clusterId=$(kubectl get ns kube-system -o=jsonpath='{.metadata.uid}')
+curl -X POST \
+-d "name=$name&email=$emailAddress&product=kubedb-enterprise&cluster=$clusterId&tos=true&token=$kubeDbApiToken" \
+https://license-issuer.appscode.com/issue-license > $HOME/$CLUSTER_NAME/$CLUSTER_NAME-kubedb-license-key.txt
