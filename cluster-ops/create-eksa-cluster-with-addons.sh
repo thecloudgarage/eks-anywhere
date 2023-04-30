2560682d66330c2ea9de7e6ffb5f26932d4894f3
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-04-30 07:56:38 +0530

    Update create-eksa-cluster-with-addons.sh

--- cluster-ops/create-eksa-cluster-with-addons.sh
+++ cluster-ops/create-eksa-cluster-with-addons.sh
@@ -8,17 +8,20 @@ echo -e "${RED}#################################################################
 read -p 'Workload cluster name: ' workloadClusterName
 read -p 'Management cluster name: ' mgmtClusterName
 read -p 'staticIp for API server High Availability: ' apiServerIpAddress
-read -p 'Cluster template filename without extension: ' clusterTemplateFileName
+echo -e "${RED}#######################IMPORTANT NOTE#################################${NC}"
+echo -e "${RED}Please copy the filename of the template intended for the cluster${NC}"
+echo -e "${RED}######################################################################${NC}"
+ls $HOME/eks-anywhere/cluster-samples/*.y* |xargs -n1 basename
+read -p 'Cluster template filename: ' clusterTemplateFileName
 read -p 'Kubernetes version 1.21, 1.22, v1.23, etc.: ' kubernetesVersion
 if ping -c 1 $staticIp &> /dev/null
 then
   echo "Error., cannot contintue...Static IP conflict, address in use"
 else
-cp $HOME/eks-anywhere/cluster-samples/$clusterTemplateFileName.yaml $HOME/$workloadClusterName-eks-a-cluster.ya
-ml
 if [ "$mgmtClusterName" == "$workloadClusterName" ]
 then
 cd $HOME
+cp $HOME/eks-anywhere/cluster-samples/$clusterTemplateFileName $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/workload-cluster-name/$workloadClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/management-cluster-name/$mgmtClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/api-server-ip/$apiServerIpAddress/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
@@ -26,6 +29,7 @@ sed -i "s/1.21/$kubernetesVersion/g" $HOME/$workloadClusterName-eks-a-cluster.ya
 eksctl anywhere create cluster -f $HOME/$workloadClusterName-eks-a-cluster.yaml 2> >(grep -v "missing")
 else
 cd $HOME
+cp $HOME/eks-anywhere/cluster-samples/$clusterTemplateFileName $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/workload-cluster-name/$workloadClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/management-cluster-name/$mgmtClusterName/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
 sed -i "s/api-server-ip/$apiServerIpAddress/g" $HOME/$workloadClusterName-eks-a-cluster.yaml
