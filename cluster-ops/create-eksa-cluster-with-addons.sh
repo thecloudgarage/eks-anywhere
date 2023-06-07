20cd919b00d9dae625f1c9afa671bfc7c57140ae
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-06-07 10:42:54 +0530

    Update create-eksa-cluster-with-addons.sh

--- cluster-ops/create-eksa-cluster-with-addons.sh
+++ cluster-ops/create-eksa-cluster-with-addons.sh
@@ -14,7 +14,7 @@ echo -e "${RED}#################################################################
 ls $HOME/eks-anywhere/cluster-samples/*.y* |xargs -n1 basename
 read -p 'Cluster template filename: ' clusterTemplateFileName
 read -p 'Kubernetes version 1.21, 1.22, 1.23, etc.: ' kubernetesVersion
-if ping -c 1 $staticIp &> /dev/null
+if ping -c 1 $apiServerIpAddress &> /dev/null
 then
   echo "Error., cannot contintue...Static IP conflict, address in use"
 else
