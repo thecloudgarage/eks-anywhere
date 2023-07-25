c707f77c633cd6dfb684f36dd83e621d798aa3be
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-07-25 17:15:29 +0530

    Update eksa-admin-machine-bootstrap-utils.sh

--- eksa-admin-machine/terraform/scripts/eksa-admin-machine-bootstrap-utils.sh
+++ eksa-admin-machine/terraform/scripts/eksa-admin-machine-bootstrap-utils.sh
@@ -9,7 +9,8 @@ sudo chmod +x /usr/local/bin/docker-compose
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+echo -e "" | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
 sudo usermod -aG docker ubuntu
