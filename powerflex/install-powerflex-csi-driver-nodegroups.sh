edf3608f694bfd2258f68c11c7034a459e5aa643
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-11-06 19:28:37 +0530

    Update install-powerflex-csi-driver-nodegroups.sh

--- powerflex/install-powerflex-csi-driver-nodegroups.sh
+++ powerflex/install-powerflex-csi-driver-nodegroups.sh
@@ -6,7 +6,7 @@
 #aws 172.26.2.9,172.26.2.11,172.26.2.49
 echo "Enter Cluster Name on which CSI driver needs to be installed"
 read -p 'clusterName: ' clusterName
-echo "Enter PowerFlex CSI release version, e.g. 2.2.0, 2.3.0, 2.4.0, 2.5.0"
+echo "Enter PowerFlex CSI release version, e.g. 2.2.0, 2.3.0, 2.4.0, 2.5.0, 2.6.0"
 read -p 'csiReleaseNumber: ' csiReleaseNumber
 echo "Enter IP or FQDN of the powerflex cluster"
 read -p 'ipOrFqdnOfPowerFlexCluster: ' ipOrFqdnOfpowerflexCluster
@@ -31,8 +31,8 @@ cd $HOME/$clusterName/csi-powerflex
 #eksdistroversion=$(kubectl get nodes -o=jsonpath='{.items[0].status.nodeInfo.kubeletVersion}')
 eksdistroversion=$(kubectl version -o json | jq -r '.serverVersion.gitVersion')
 rm -rf helm/csi-vxflexos/Chart.yaml
-wget https://raw.githubusercontent.com/thecloudgarage/eks-anywhere/main/powerflex/$eksdistroversion-Chart.yaml
-mv $eksdistroversion-Chart.yaml helm/csi-vxflexos/Chart.yaml
+wget https://raw.githubusercontent.com/thecloudgarage/eks-anywhere/main/powerflex/$csiReleaseNumber-Chart.yaml
+mv $csiReleaseNumber-Chart.yaml helm/csi-vxflexos/Chart.yaml
 sed -i "s/eksDistroVersion/$eksdistroversion/g" helm/csi-vxflexos/Chart.yaml
 cd $HOME/$clusterName/csi-powerflex
 git clone https://github.com/kubernetes-csi/external-snapshotter/
