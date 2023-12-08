5f3e7e4a35955ea128c783346b313f6cd70aa14b
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-12-08 13:04:43 +0530

    Update restore-mysql-source-pvc.sh

--- mysql/standalone/powerflex/restore-mysql-source-pvc.sh
+++ mysql/standalone/powerflex/restore-mysql-source-pvc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 read -p 'volumeSnapshotName: ' volumeSnapshotName
+rm -rf restore-mysql-source-pvc-sample.yaml
+rm -rf restore-mysql-source-pvc.ya*
 wget https://raw.githubusercontent.com/thecloudgarage/eks-anywhere/main/mysql/standalone/powerflex/restore-mysql-source-pvc-sample.yaml
 cp restore-mysql-source-pvc-sample.yaml restore-mysql-source-pvc.yaml
 sed -i "s/volumeSnapshotName/$volumeSnapshotName/g" restore-mysql-source-pvc.yaml
