082f9ea9decbbd6eb2eeedadd6b1d3ca2d70835f
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-12-08 13:03:57 +0530

    Update create-snapshot.sh

--- mysql/standalone/powerflex/create-snapshot.sh
+++ mysql/standalone/powerflex/create-snapshot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 NOW=$(date "+%Y%m%d%H%M%S")
+rm -rf snapshot-sample.yaml
+rm -rf snapshot.ya*
 wget https://raw.githubusercontent.com/thecloudgarage/eks-anywhere/main/mysql/standalone/powerflex/snapshot-sample.yaml
 cp snapshot-sample.yaml snapshot.yaml
 sed -i "s/datetime/$NOW/g" snapshot.yaml
