b7d857946e6394be16c3eaee58b1244091196a58
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-12-08 19:20:47 +0530

    Update restore-mysql-source-pvc.sh

--- mysql/standalone/powerflex/restore-mysql-source-pvc.sh
+++ mysql/standalone/powerflex/restore-mysql-source-pvc.sh
@@ -6,6 +6,7 @@ kubectl delete -f mysql-restored-source-cluster.yaml
 kubectl delete -f mysql-restored-pvc-source-cluster.yaml
 rm -rf mysql-restored-pvc-source-cluster.yaml
 rm -rf mysql-restored-pvc-source-cluster.yaml*
+rm -rf mysql-restored-pvc-source-cluster-sample*
 wget https://raw.githubusercontent.com/thecloudgarage/eks-anywhere/main/mysql/standalone/powerflex/mysql-restored-pvc-source-cluster-sample.yaml
 cp mysql-restored-pvc-source-cluster-sample.yaml mysql-restored-pvc-source-cluster.yaml
 sed -i "s/volumeSnapshotName/$volumeSnapshotName/g" mysql-restored-pvc-source-cluster.yaml
