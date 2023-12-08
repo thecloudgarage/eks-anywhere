190b103c6550f63756169c46a118767fa47612bf
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2023-12-08 10:12:46 +0530

    Create create-snapshot.sh

---
+++ mysql/standalone/powerflex/create-snapshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NOW=$(date "+%Y%m%d%H%M%S")
+https://raw.githubusercontent.com/thecloudgarage/eks-anywhere/main/mysql/standalone/powerflex/snapshot-sample.yaml
+cp snapshot-sample.yaml snapshot.yaml
+sed -i "s/datetime/$NOW/g" snapshot.yaml
+kubectl create -f snapshot.yaml
