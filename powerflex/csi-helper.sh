4f87e456f4250efec9ca971e61147391843d9c76
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2024-03-01 20:08:11 +0530

    Update csi-helper.sh

--- powerflex/csi-helper.sh
+++ powerflex/csi-helper.sh
@@ -1,3 +1,4 @@
+####REMOVE THE INIT SDC CONTAINER
 sed -i '/^ *\initContainers:/{
 :sub;
   $b eof;
@@ -8,4 +9,12 @@ sed -i '/^ *\initContainers:/{
   t loop;
   d;
 :loop; n; b loop;
-}' dell-helm-charts/charts/csi-vxflexos/templates node.yaml
+}' dell-helm-charts/charts/csi-vxflexos/templates/node.yaml
+#
+####ADD NODESELECTORS
+sed -i '/nodeSelector:$/r'<(
+echo "group: node-selector-group-name"
+) ./dell-helm-charts/charts/csi-vxflexos/values.yaml
+#
+sed -i "s/^kubeVersion.*/kubeVersion: \"${eksdistroversion}\"/g" ./dell-helm-charts/charts/csi-vxflexos/Chart.yaml
+#
