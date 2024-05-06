a15114c71d58b61e6c502047459a11774d25ca1b
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2024-05-06 11:31:56 +0530

    Update image_builder_govc.sh

--- eksa-admin-machine/terraform/scripts/image_builder_govc.sh
+++ eksa-admin-machine/terraform/scripts/image_builder_govc.sh
@@ -2,11 +2,15 @@
 mkdir -p /home/image-builder/.ssh
 sudo mkdir -p /tmp/eks-image-builder-cni
 sudo chown -R image-builder:image-builder /tmp/eks-image-builder-cni
-sudo apt install jq unzip make -y
+sudo apt install jq yq unzip make ansible -y
 sudo apt install python3 python3-setuptools python3-dev python3-pip -y
 echo "HostKeyAlgorithms +ssh-rsa" >> /home/$USER/.ssh/config
 echo "PubkeyAcceptedKeyTypes +ssh-rsa" >> /home/$USER/.ssh/config
 sudo chmod 600 /home/image-builder/.ssh/config
-python3 -m pip install --user ansible
-curl -L -o - "https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz" | sudo tar -C /usr/local/bin -xvzf - govc
+sudo curl -L -o - https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz | sudo tar -C /usr/local/bin -xvzf - govc
+EKSA_RELEASE_VERSION=$(curl -sL https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.latestVersion")
+BUNDLE_MANIFEST_URL=$(curl -s https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.releases[] | select(.version==\"$EKSA_RELEASE_VERSION\").bundleManifestUrl")
+IMAGEBUILDER_TARBALL_URI=$(curl -s $BUNDLE_MANIFEST_URL | yq ".spec.versionsBundles[0].eksD.imagebuilder.uri")
+curl -s $IMAGEBUILDER_TARBALL_URI | tar xz ./image-builder
+sudo install -m 0755 ./image-builder /usr/local/bin/image-builder   
 exit
