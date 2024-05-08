82df2661b8534a1f8aa77fdae1be1cfbdd2f24d3
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2024-05-08 08:48:39 +0530

    Update image_builder_govc.sh

--- eksa-admin-machine/terraform/scripts/image_builder_govc.sh
+++ eksa-admin-machine/terraform/scripts/image_builder_govc.sh
@@ -13,7 +13,7 @@ EKSA_RELEASE_VERSION=$(curl -sL https://anywhere-assets.eks.amazonaws.com/releas
 BUNDLE_MANIFEST_URL=$(curl -s https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.releases[] | select(.version==\"$EKSA_RELEASE_VERSION\").bundleManifestUrl")
 IMAGEBUILDER_TARBALL_URI=$(curl -s $BUNDLE_MANIFEST_URL | yq ".spec.versionsBundles[0].eksD.imagebuilder.uri")
 echo $IMAGEBUILDER_TARBALL_URI > /home/image-builder/imagebuilder-tarball-uri.txt
-sudo curl -s $IMAGEBUILDER_TARBALL_URI | sudo tar xz ./image-builder
+curl -s $IMAGEBUILDER_TARBALL_URI | tar xz ./image-builder
 sudo cp image-builder /usr/local/bin
 #sudo install -m 0755 ./image-builder /usr/local/bin/image-builder
 cd -
