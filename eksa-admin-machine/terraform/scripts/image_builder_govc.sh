2b6847c426e143e4a380d886bec804e55dd53e9b
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2024-05-08 09:02:07 +0530

    Update image_builder_govc.sh

--- eksa-admin-machine/terraform/scripts/image_builder_govc.sh
+++ eksa-admin-machine/terraform/scripts/image_builder_govc.sh
@@ -9,9 +9,9 @@ echo "HostKeyAlgorithms +ssh-rsa" >> /home/image-builder/.ssh/config
 echo "PubkeyAcceptedKeyTypes +ssh-rsa" >> /home/image-builder/.ssh/config
 sudo chmod 600 /home/image-builder/.ssh/config
 sudo curl -L -o - https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz | sudo tar -C /usr/local/bin -xvzf - govc
-EKSA_RELEASE_VERSION=$(curl -sL https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.latestVersion")
-BUNDLE_MANIFEST_URL=$(curl -s https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.releases[] | select(.version==\"$EKSA_RELEASE_VERSION\").bundleManifestUrl")
-IMAGEBUILDER_TARBALL_URI=$(curl -s $BUNDLE_MANIFEST_URL | yq ".spec.versionsBundles[0].eksD.imagebuilder.uri")
+EKSA_RELEASE_VERSION=$(sudo curl -sL https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.latestVersion")
+BUNDLE_MANIFEST_URL=$(sudo curl -s https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.releases[] | select(.version==\"$EKSA_RELEASE_VERSION\").bundleManifestUrl")
+IMAGEBUILDER_TARBALL_URI=$(sudo curl -s $BUNDLE_MANIFEST_URL | yq ".spec.versionsBundles[0].eksD.imagebuilder.uri")
 echo $IMAGEBUILDER_TARBALL_URI > /home/image-builder/imagebuilder-tarball-uri.txt
 curl -s $IMAGEBUILDER_TARBALL_URI | tar xz ./image-builder
 sudo cp image-builder /usr/local/bin
