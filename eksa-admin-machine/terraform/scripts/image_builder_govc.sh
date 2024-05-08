4a02e641fd479836e677f99af3da2b071c7a9ea6
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2024-05-08 08:09:43 +0530

    Update image_builder_govc.sh

--- eksa-admin-machine/terraform/scripts/image_builder_govc.sh
+++ eksa-admin-machine/terraform/scripts/image_builder_govc.sh
@@ -5,8 +5,8 @@ sudo chown -R image-builder:image-builder /tmp/eks-image-builder-cni
 sudo apt install jq unzip make ansible -y
 sudo snap install yq -y
 sudo apt install python3 python3-setuptools python3-dev python3-pip -y
-echo "HostKeyAlgorithms +ssh-rsa" >> /home/$USER/.ssh/config
-echo "PubkeyAcceptedKeyTypes +ssh-rsa" >> /home/$USER/.ssh/config
+echo "HostKeyAlgorithms +ssh-rsa" >> /home/image-builder/.ssh/config
+echo "PubkeyAcceptedKeyTypes +ssh-rsa" >> /home/image-builder/.ssh/config
 sudo chmod 600 /home/image-builder/.ssh/config
 sudo curl -L -o - https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz | sudo tar -C /usr/local/bin -xvzf - govc
 EKSA_RELEASE_VERSION=$(curl -sL https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.latestVersion")
