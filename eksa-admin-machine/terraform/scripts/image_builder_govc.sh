bcfd8af3794f74ecdb10a25ea4809898b90898dd
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2024-05-08 10:19:59 +0530

    Update image_builder_govc.sh

--- eksa-admin-machine/terraform/scripts/image_builder_govc.sh
+++ eksa-admin-machine/terraform/scripts/image_builder_govc.sh
@@ -2,9 +2,10 @@
 mkdir -p /home/image-builder/.ssh
 sudo mkdir -p /tmp/eks-image-builder-cni
 sudo chown -R image-builder:image-builder /tmp/eks-image-builder-cni
-sudo apt install jq unzip make ansible -y
+sudo apt install jq unzip make ansible python3 python3-setuptools python3-dev python3-pip -y
 sudo snap install yq -y
-sudo apt install python3 python3-setuptools python3-dev python3-pip -y
+echo "packages installed" > /home/image-builder/done.txt
+#sudo apt install python3 python3-setuptools python3-dev python3-pip -y
 echo "HostKeyAlgorithms +ssh-rsa" >> /home/image-builder/.ssh/config
 echo "PubkeyAcceptedKeyTypes +ssh-rsa" >> /home/image-builder/.ssh/config
 sudo chmod 600 /home/image-builder/.ssh/config
